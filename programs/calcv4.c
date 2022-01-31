bc3d477c76114bcc3a4bf2f775bda434f586ea2a
YazawaKenichi <tonfarken83825@gmail.com>
2022-01-31 10:50:34 +0900

    calclator.c

---
+++ programs/calcv4.c
@@ -0,0 +1,44 @@
+// センサの設置位置の算出
+
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+    unsigned short int sensorcount[2] = {12, 4};   // 手前センサの数
+
+    // 角度
+    long double degree[2] = {90, 18.6};    // 配置可能角度
+    // 半径
+    long double radius[2] = {55, 150};    // 半径
+
+    // 変数
+    int j;
+    long double theta, r;
+    long double x, y;
+
+//    printf("%d\n", sensorcount * 2);
+    for(int k = 0; k < 2; k++)
+    {
+        for(int i = 1; i <= sensorcount[k] * 2; i++)
+        {
+    //        printf("i = %d\n", i);
+            if(i % 2 == 1)
+            {
+                j = i / 2 + 1;
+                theta = 2 * M_PI * (90 + degree[k] / 2 - i * degree[k] / sensorcount[k] / 2) / 360;
+                r = radius[k];
+                printf("\033[0m");
+                x = r * cos(theta) / 100;
+                y = r * sin(theta) / 100;
+                theta = theta * 360 / 2 / M_PI;
+                printf("    (at %Lf %Lf)\n", x * 100, y * -100);
+                theta = theta * 2 * M_PI / 360;
+            }
+        }
+        printf("\n");
+    }
+}
+
+
+
