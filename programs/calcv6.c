461682112b9a1390ada3a02927db5851c94e14ec
Yazawa <37500115+YazawaKenichi@users.noreply.github.com>
2022-04-08 09:52:05 +0900

    Update calcv6.c

--- programs/calcv6.c
+++ programs/calcv6.c
@@ -1,4 +1,5 @@
 // センサの設置位置の算出
+// Oda くん向け
 
 #include <stdio.h>
 #include <math.h>
@@ -13,24 +14,26 @@ void main()
     long double radius = 55;    // 半径
 
     // 変数
-    int j;
     long double theta, r;
     long double x, y;
 
 //    printf("%d\n", sensorcount * 2);
     for(int i = 1; i <= sensorcount * 2; i++)
     {
+        // 等間隔のパディングを持たせたいので、センサとセンサの間にもセンサがあるように見せかけてプログラムする。
+        // よって 実際に配置するセンサの数 * 2 個のセンサがあるものとして考える。
+        
 //        printf("i = %d\n", i);
         if(i % 2 == 1)
         {
-            j = i / 2 + 1;
+            // i が奇数の時に実際に配置するセンサの座標と角度を求める。
             theta = 2 * M_PI * (90 + degree / 2 - i * degree / sensorcount / 2) / 360;
             r = radius;
             printf("\033[0m");
             x = r * cos(theta) / 100;
             y = r * sin(theta) / 100;
             theta = theta * 360 / 2 / M_PI;
-            printf("    (at %Lf %Lf)\n", x * 100, y * -100);
+            printf("(%Lf, %Lf), \n", x * 100, y * -100);
             theta = theta * 2 * M_PI / 360;
         }
     }
